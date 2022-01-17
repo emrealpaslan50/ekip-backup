d88fb336de72cb3b3eb186c1ac1a9e40fc931e92
emrealpaslan50 <76908075+emrealpaslan50@users.noreply.github.com>
2022-01-17 21:59:10 +0200

    Add files via upload

---
+++ başlat.bat
@@ -0,0 +1,8 @@
+@echo off
+color 0f
+title Rage-Backup
+echo Dokun
+PAUSE
+:a
+node index.js
+goto a
---
+++ commands/eval.js
@@ -0,0 +1,62 @@
+const Discord = require("discord.js");
+
+exports.run = async (client, message, args) => {
+  if (global.config.owners.includes(message.author.id) === false)
+    return message.channel
+      .send(`**Bu komutu sadece \`NevşehirLii\` kullanabilir!**`)
+      .then(x => x.delete({ timeout: 5000 }));
+  let msg = message;
+
+  let code = args.join(" ");
+  if (!code) return message.channel.send("Kod belirt!");
+
+  const clean = text => {
+    if (typeof text !== "string")
+      text = require("util").inspect(text, { depth: 0 });
+    text = text
+      .replace(/`/g, "`" + String.fromCharCode(8203))
+      .replace(/@/g, "@" + String.fromCharCode(8203))
+      .replace(new RegExp(client.token, "g"), "NevşehirLii Was Here?");
+
+    return text;
+  };
+  try {
+    var evaled = await eval(code);
+    return message.channel
+      .send(clean(evaled), {
+        code: "js",
+        split: true
+      })
+      .catch(e => {
+        return message.channel.send(e, {
+          split: true,
+          code: "xl"
+        });
+      });
+  } catch (e) {
+    return message.channel
+      .send(e, {
+        split: true,
+        code: "xl"
+      })
+      .catch(err => {
+        return message.channel.send(err, {
+          split: true,
+          code: "xl"
+        });
+      });
+  }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ["eval", "evsat", "kod"],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "eval",
+  description: "Yazılan kodu çalıştırır.",
+  usage: "eval <kod>"
+};
